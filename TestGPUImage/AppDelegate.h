2128ed1e1f10bc83f9eb2e8fbec6cca02771839f
bhjdfvhuisdh <18867148901@163.com>
2016-10-20 14:43:39 +0800

    Initial Commit

---
+++ TestGPUImage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestGPUImage
+//
+//  Created by 黄鑫 on 16/10/20.
+//  Copyright © 2016年 hx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
